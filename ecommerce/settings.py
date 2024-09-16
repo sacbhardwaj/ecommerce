9f2624de8d7ae0597c3101abf8bfca88c54a09c4
sacbhardwaj <89528159+sacbhardwaj@users.noreply.github.com>
2024-09-16 18:55:06 +0530

    commit #2

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER='sachinsharma935039@gmail.com'
-EMAIL_HOST_PASSWORD='ovwdcthvvaliwcgy'
+EMAIL_HOST_PASSWORD=''
 EMAIL_PORT=587
 # EMAIL_USE_SSL = True  
 EMAIL_USE_TLS=True
